17489c4fc3ea4bbcda2184fca8b868207ee59193
keisuke.sato <kesuike1224@icloud.com>
2021-01-23 17:18:55 +0900

    Add CreateServlet.java

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import Task.java.task;
+import models.Task;
 import utils.DBUtil;
 
 /**
@@ -35,7 +35,7 @@ public class CreateServlet extends HttpServlet {
         if(_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
 
-            task m = new task();
+            Task m = new Task();
 
             String title = request.getParameter("title");
             m.setTitle(title);
